207a07e6f574f970a2a2b687893e83700e258fb3
Dmitriy <team2tiblo@gmail.com>
2026-01-19 16:50:18 +0300

    iterator added

--- files_prac/file_manager/src/main.rs
+++ files_prac/file_manager/src/main.rs
@@ -6,9 +6,8 @@ use std::error::Error;
 
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem passing arguments {err}");
         process::exit(1);
     });
@@ -46,13 +45,18 @@ pub struct Config {
     ignore_case: bool,
 }
 impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    fn build(mut args: impl Iterator<Item = String>,) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
